cfb37b3c3df92e3f31d7e7fe36b9242fcc5486ac
juanlu-ms <juanlums11@gmail.com>
2025-01-31 18:24:20 +0100

    prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,8 @@
-const { Pressable } = require("react-native");
-
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: ['expo', prettier],
-  plugins: ['prettier'],
+  extends: ["expo", "prettier"],
+  plugins: ["prettier"],
   rules: {
-    'prettier/prettier': 'error',
+    "prettier/prettier": "error",
   },
 };
