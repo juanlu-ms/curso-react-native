c970c7e39d930b57238ea7cb53a29e52e8a92713
juanlu-ms <juanlums11@gmail.com>
2025-02-10 02:26:44 +0100

    Agregar esquema de Metacritic, crear componentes de Layout e Index, y actualizar la entrada principal en package.json

---
+++ app/_layout.js
@@ -0,0 +1,10 @@
+import { Slot } from "expo-router";
+import { View, Text } from "react-native";
+
+export default function Layout() {
+	return (
+		<View className="flex-1 bg-black items-center justify-center">
+			<Slot />
+		</View>
+	);
+}
