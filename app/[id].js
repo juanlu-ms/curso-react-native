1951324596c16db3727e452c73c72210c40d63fa
juanlu-ms <juanlums11@gmail.com>
2025-02-11 18:07:24 +0100

    Obtención del Id del juego mediante useLocalSearchParams()

--- app/[id].js
+++ app/[id].js
@@ -1,12 +1,15 @@
 import { Link } from "expo-router";
 import { Text, View } from "react-native";
+import { useLocalSearchParams } from "expo-router";
 
 export default function Detail() {
+	const { id } = useLocalSearchParams();
+
 	return (
-		<View className="flex-1 justify-center items-center">
+		<View className="flex-1 justify-center items-center bg-black">
 			<View>
 				<Text className="text-white font-bold mb-8 text-2xl">
-					Detalle del juego
+					Detalle del juego {id}
 				</Text>
 				<Link href="/" className="text-blue-400">
 					Volver a inicio
