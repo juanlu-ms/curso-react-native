1b0029bad801e668a821261192810e659fb682c5
juanlu-ms <juanlums11@gmail.com>
2025-02-11 18:13:00 +0100

    Reemplazar el componente Slot por Stack en el layout y ajustar estilos del encabezado

--- app/_layout.js
+++ app/_layout.js
@@ -1,10 +1,16 @@
-import { Slot } from "expo-router";
+import { Stack } from "expo-router";
 import { View } from "react-native";
 
 export default function Layout() {
 	return (
-		<View className="flex-1 bg-black items-center justify-center">
-			<Slot />
+		<View className="flex-1">
+			<Stack
+				initialRouteName="index"
+				screenOptions={{
+					headerStyle: { backgroundColor: "black" },
+					headerTintColor: "white",
+				}}
+			/>
 		</View>
 	);
 }
