2c08058b92d45251e1963d3c7e23ae3d277ffce6
juanlu-ms <juanlums11@gmail.com>
2025-02-13 17:46:17 +0100

    Añadir barra de estado al componente Layout

--- app/_layout.js
+++ app/_layout.js
@@ -1,26 +1,30 @@
 import { Stack, Link } from "expo-router";
 import { View, Pressable } from "react-native";
+import { StatusBar } from "expo-status-bar";
 import { Logo, CircleInfoIcon } from "../components/Icons";
 
 export default function Layout() {
 	return (
-		<View className="flex-1">
-			<Stack
-				initialRouteName="index"
-				screenOptions={{
-					headerStyle: { backgroundColor: "black" },
-					headerTintColor: "white",
-					headerTitle: "",
-					headerLeft: () => <Logo />,
-					headerRight: () => (
-						<Link asChild href={"/about"}>
-							<Pressable className={`active:opacity-50`}>
-								<CircleInfoIcon />
-							</Pressable>
-						</Link>
-					),
-				}}
-			/>
-		</View>
+		<>
+			<StatusBar style="light" backgroundColor="black" />
+			<View className="flex-1">
+				<Stack
+					initialRouteName="index"
+					screenOptions={{
+						headerStyle: { backgroundColor: "black" },
+						headerTintColor: "white",
+						headerTitle: "",
+						headerLeft: () => <Logo />,
+						headerRight: () => (
+							<Link asChild href={"/about"}>
+								<Pressable className={`active:opacity-50`}>
+									<CircleInfoIcon />
+								</Pressable>
+							</Link>
+						),
+					}}
+				/>
+			</View>
+		</>
 	);
 }
